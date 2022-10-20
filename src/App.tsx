aa2a441f0fff1796557da6eb4910d47c5fc658ce
Cristina Fores <cristinaforescampos1992@gmail.com>
2022-10-21 01:14:30 +0200

    Add index calling

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import "./index.css";
 import Keyboard from "./components/Keyboard/Keyboard";
 import Display from "./components/Display/Display";
 import Actions from "./components/Actions/Actions";
+import ActionCalling from "./components/ActionCalling/ActionCalling";
 
 const App = (): JSX.Element => {
   return (
     <>
       <div className="container">
+        <ActionCalling />
         <main className="phone">
           <div className="keyboard-container">
             <Keyboard />
