b98e5d613c929a06cff2814a34b046708109a3df
Cristina Fores <cristinaforescampos1992@gmail.com>
2022-10-20 19:20:44 +0200

    Create Key and add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import "./index.css";
+import React, { useEffect, useContext } from "react";
+import Key from "./components/Key";
+
+import numbers from "./data/numbers";
+import PhoneContext from "./components/contexts/PhoneContext";
+
 const App = (): JSX.Element => {
+  const { loadNumbers } = useContext(PhoneContext);
+
+  useEffect(() => {
+    loadNumbers(numbers);
+  }, [loadNumbers]);
+
   return (
-    <div className="container">
-      <p>hola</p>
-    </div>
+    <>
+      <div className="container">
+        <Key numbersKey={numbers[8]} action={() => {}} />
+        <span className="message">Calling...</span>
+        <main className="phone">
+          <div className="keyboard-container"></div>
+          <div className="actions">
+            <span className="number">667359961</span>
+          </div>
+        </main>
+      </div>
+    </>
   );
 };
 
--- src/data/numbers.ts
+++ src/data/numbers.ts
@@ -1,3 +1,3 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
 export default numbers;
